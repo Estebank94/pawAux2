5481761e8a7b08554114331c1a0c04ba9802cb94
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-08-26 15:52:16 -0300

    form @requestMapping

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -3,7 +3,9 @@ package ar.edu.itba.paw.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.itba.paw.interfaces.UserService;
@@ -19,6 +21,14 @@ public class HelloWorldController {
 	public ModelAndView helloWorld() {
 		final ModelAndView mav = new ModelAndView("index");
 		mav.addObject("greeting", "PAW");
+		return mav;
+	}
+
+	@RequestMapping("/processForm")
+	/*we map the web request, if they go to my website they will see THIS*/
+	public ModelAndView processForm(@RequestParam("searchName") String search, Model model) {
+		final ModelAndView mav = new ModelAndView("specialists");
+		model.addAttribute("message", search);
 		return mav;
 	}
 }
