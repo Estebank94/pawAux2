e8f2b850969c9c23faef6f55ee01b1f36a1b8515
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-08-31 16:12:18 -0300

    Doctor Dao Implementation

---
+++ paw2018b/persistence/src/main/java/ar/edu/itba/paw/DoctorDaoImpl.java
@@ -0,0 +1,47 @@
+package ar.edu.itba.paw;
+
+import ar.edu.itba.paw.interfaces.DoctorDao;
+import ar.edu.itba.paw.models.Doctor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class DoctorDaoImpl implements DoctorDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final RowMapper<Doctor> ROW_MAPPER = new RowMapper<Doctor>(){
+
+        @Override
+        public Doctor mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Doctor(rs.getString("firstName"), rs.getString("lastName"), rs.getString("sex"),
+                    rs.getString("address"), rs.getString("avatar"), rs.getInt("id"));}
+        };
+
+        @Autowired
+        public DoctorDaoImpl(final DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+        }
+
+    @Override
+    public Optional<List<Doctor>> listDoctors() {
+            final List<Doctor> list = jdbcTemplate.query("SELECT * FROM doctor", ROW_MAPPER);
+
+            if(list.isEmpty()){
+                return Optional.empty();
+            }
+            return Optional.of(list);
+    }
+};
+
+
+
+
