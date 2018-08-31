40190c780a4c0643949cc603e2e08c0455fecd84
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-08-31 16:13:29 -0300

    no message

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -4,10 +4,8 @@ import ar.edu.itba.paw.models.Search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.itba.paw.interfaces.UserService;
