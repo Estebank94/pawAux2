1a0e2220e937024d14da908651bd789c045706f5
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-08-31 20:41:46 -0300

    Added findDoctors

--- paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/DoctorService.java
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/DoctorService.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.interfaces;
 
 import ar.edu.itba.paw.models.Doctor;
+import ar.edu.itba.paw.models.Search;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,5 +9,7 @@ import java.util.Optional;
 public interface DoctorService {
 
    Optional<List<Doctor>> listDoctors();
+
+   Optional<List<Doctor>> findDoctors(Search search);
 
 }
