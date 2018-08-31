09c0423e6935f698121738912bf95e497d0015d9
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-08-31 18:19:03 -0300

    Doctor service

---
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/DoctorService.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.paw.interfaces;
+
+import ar.edu.itba.paw.models.Doctor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DoctorService {
+
+    public Optional<List<Doctor>> listDoctors();
+
+}
