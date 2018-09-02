dced3f22bb56114bfbcb367c4234c8e17a9ff809
Unknown <Estebankramer@hotmail.com>
2018-09-02 15:45:08 -0300

    Fixed exception caused when result is empty

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaces.DoctorService;
+import ar.edu.itba.paw.models.Doctor;
 import ar.edu.itba.paw.models.Search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.itba.paw.interfaces.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HelloWorldController {
@@ -31,7 +35,19 @@ public class HelloWorldController {
 	@RequestMapping("/processForm")
 	public ModelAndView processForm(@ModelAttribute("search") Search theSearch) {
 		final ModelAndView mav = new ModelAndView("specialists");
-		mav.addObject("doctorList", doctorService.findDoctors(theSearch).get());
+		Optional<List<Doctor>> doctors =  doctorService.findDoctors(theSearch);
+		List<Doctor> doctorsList = null;
+		if(doctors.isPresent()) {
+			doctorsList = doctors.get();
+		}
+		else {
+			doctors = doctorService.listDoctors();
+			if(doctors.isPresent()) {
+				doctorsList = doctors.get();
+			}
+		}
+
+		mav.addObject("doctorList", doctorsList);
 		return mav;
 	}
 }
