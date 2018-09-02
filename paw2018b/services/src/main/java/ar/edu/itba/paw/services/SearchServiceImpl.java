926099a90e0f4111911b8722edaf2f2b5ba82c7c
Unknown <Estebankramer@hotmail.com>
2018-09-02 16:45:13 -0300

    SearchService Impl

--- paw2018b/services/src/main/java/ar/edu/itba/paw/services/SearchServiceImpl.java
+++ paw2018b/services/src/main/java/ar/edu/itba/paw/services/SearchServiceImpl.java
@@ -1,7 +1,32 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.SearchDao;
+import ar.edu.itba.paw.interfaces.SearchService;
+import ar.edu.itba.paw.models.ListItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by estebankramer on 02/09/2018.
  */
-public class SearchServiceImpl {
+
+@Service("SearchDaoImpl")
+public class SearchServiceImpl implements SearchService {
+
+    @Autowired
+    private SearchDao searchDao;
+
+    @Override
+    public Optional<List<ListItem>> listInsurances() {
+        return searchDao.listInsurances();
+    }
+
+    @Override
+    public Optional<List<ListItem>> listZones() {
+        return null;
+    }
+
 }
