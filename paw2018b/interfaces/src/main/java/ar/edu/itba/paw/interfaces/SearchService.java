70385282416cac0ba2db0f1c01b893b620caa005
Unknown <Estebankramer@hotmail.com>
2018-09-02 16:43:50 -0300

    Search Service interface

--- paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/SearchService.java
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/SearchService.java
@@ -1,7 +1,16 @@
 package ar.edu.itba.paw.interfaces;
 
+import ar.edu.itba.paw.models.ListItem;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by estebankramer on 02/09/2018.
  */
+
 public interface SearchService {
+    Optional<List<ListItem>> listInsurances();
+
+    Optional<List<ListItem>> listZones();
 }
