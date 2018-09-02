8f5e52b3d241c6895d87047e037b9dfb4226a966
Unknown <Estebankramer@hotmail.com>
2018-09-02 16:43:02 -0300

    Search Dao Interface

---
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/SearchDao.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.interfaces;
+
+/**
+ * Created by estebankramer on 02/09/2018.
+ */
+public interface SearchDao {
+}
---
+++ paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/SearchDaoImpl.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.persistence;
+
+/**
+ * Created by estebankramer on 02/09/2018.
+ */
+public class SearchDaoImpl {
+}
