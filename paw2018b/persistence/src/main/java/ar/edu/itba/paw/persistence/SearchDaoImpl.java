9c512d438a9f562e4f6c42a2d962ed78fc7b040b
Unknown <Estebankramer@hotmail.com>
2018-09-02 16:45:00 -0300

    SearchDaoImpl

--- paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/SearchDaoImpl.java
+++ paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/SearchDaoImpl.java
@@ -1,7 +1,53 @@
 package ar.edu.itba.paw.persistence;
 
+import ar.edu.itba.paw.interfaces.SearchDao;
+import ar.edu.itba.paw.models.ListItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by estebankramer on 02/09/2018.
  */
-public class SearchDaoImpl {
+
+@Repository
+public class SearchDaoImpl implements SearchDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final RowMapper<ListItem> ROW_MAPPER = new RowMapper<ListItem>(){
+
+        @Override
+        public ListItem mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new ListItem( rs.getString("insuranceName"), rs.getInt("id"));
+        }
+    };
+
+    @Autowired
+    public SearchDaoImpl(final DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Override
+    public Optional<List<ListItem>> listInsurances() {
+
+        final List<ListItem> list = jdbcTemplate.query("SELECT * FROM insurance", ROW_MAPPER);
+
+        if(list.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(list);
+    }
+
+    @Override
+    public Optional<List<ListItem>> listZones() {
+        return null;
+    }
 }
