9ff524e6be962196013a9b1cfb69eda75639a6f6
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-21 19:39:55 -0300

    new controller for registration

---
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegistrationController.java
@@ -0,0 +1,65 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.interfaces.SearchService;
+import ar.edu.itba.paw.webapp.forms.PersonalForm;
+import ar.edu.itba.paw.webapp.forms.ProfessionalForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private SearchService searchService;
+
+    @RequestMapping(value="/doctorRegistration", method = { RequestMethod.POST })
+    public ModelAndView doctorRegistration (@Valid @ModelAttribute("personal") PersonalForm personalForm, final BindingResult errors){
+
+        if(errors.hasErrors()){
+            return showDoctorRegistration(personalForm);
+        }else{
+            //create Doctor
+            final ModelAndView mav = new ModelAndView("registerSpecialist2");
+            mav.addObject("professional", new ProfessionalForm());
+            mav.addObject("insuranceList", searchService.listInsurances().get());
+            return mav;
+        }
+
+    }
+
+    @RequestMapping(value="/showDoctorRegistration", method = { RequestMethod.GET })
+    public ModelAndView showDoctorRegistration (@ModelAttribute("personal") PersonalForm personalForm){
+
+        final ModelAndView mav = new ModelAndView("registerSpecialist");
+        return mav;
+    }
+
+    @RequestMapping(value = "/showDoctorProfile", method = {RequestMethod.GET})
+    public ModelAndView showDoctorProfile(@ModelAttribute("professional")ProfessionalForm professionalForm){
+
+        final ModelAndView mav = new ModelAndView("registerSpecialist2");
+        mav.addObject("insuranceList", searchService.listInsurances().get());
+        return mav;
+    }
+
+    @RequestMapping(value = "/doctorProfile", method = {RequestMethod.POST})
+    public ModelAndView doctorProfile (@Valid @ModelAttribute("professional") ProfessionalForm professionalForm, final BindingResult errors){
+
+        if(errors.hasErrors()){
+            return showDoctorProfile(professionalForm);
+        }
+        System.out.println(professionalForm.getLanguages());
+        final ModelAndView mav = new ModelAndView("finalStep");
+        return mav;
+    }
+
+}
