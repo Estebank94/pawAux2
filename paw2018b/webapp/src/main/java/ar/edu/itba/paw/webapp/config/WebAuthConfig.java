2802612b52f1e2fd89906e917e26ec210daaa0a9
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-22 19:31:19 -0300

    Working on Log in

---
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -0,0 +1,38 @@
+package ar.edu.itba.paw.webapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("ar.edu.itba.paw.webapp")
+public class WebAuthConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+       //add our users for in memory authentication
+       auth.inMemoryAuthentication().withUser("martina").password("martina").roles("ADMIN");
+       auth.inMemoryAuthentication().withUser("esteban").password("esteban").roles("ADMIN");
+       auth.inMemoryAuthentication().withUser("oliver").password("oliver").roles("EMPLOYEE");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/favicon.ico", "/403");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().
+                loginPage("/showLogIn").loginProcessingUrl("/authenticateUser").permitAll();
+    }
+}
