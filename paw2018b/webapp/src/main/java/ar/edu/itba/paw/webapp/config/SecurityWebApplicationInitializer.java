326607c243ff7481cbff9962e6771fbc0f4e26ff
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-22 22:20:01 -0300

    not starting the webapp with log in, so this class is not needed

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.itba.paw.webapp.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
