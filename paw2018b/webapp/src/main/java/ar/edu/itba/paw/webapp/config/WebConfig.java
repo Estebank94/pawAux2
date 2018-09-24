d80aba1416e96f067eef7d04a652649fe256771e
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-23 21:34:25 -0300

    webconfig

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,12 +1,16 @@
 package ar.edu.itba.paw.webapp.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.nio.charset.StandardCharsets;
 
@@ -21,6 +26,9 @@ import java.nio.charset.StandardCharsets;
 @ComponentScan({ "ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.services", "ar.edu.itba.paw.persistence" })
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+//	@Value("classpath:schema.sql")
+//	private Resource schemaSQL;
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -48,6 +56,20 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		ds.setPassword("67wFYxljg");
 		return ds;
 	}
+
+//	@Bean
+//	public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
+//		final DataSourceInitializer dsi = new DataSourceInitializer();
+//		dsi.setDataSource(ds);
+//		dsi.setDatabasePopulator(databasePopulator());
+//		return dsi;
+//	}
+//
+//	private DatabasePopulator databasePopulator(){
+//		final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
+////		dbp.addScript(schemaSQL);
+//		return dbp;
+//	}
 
 	@Bean
 	public MessageSource messageSource (){
