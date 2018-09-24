abf970899815dd0d748f007a0347b3ed780e3107
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-24 17:45:25 -0300

    TODOS

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegistrationController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import ar.edu.itba.paw.interfaces.SearchService;
 import ar.edu.itba.paw.models.Doctor;
 import ar.edu.itba.paw.webapp.forms.PersonalForm;
 import ar.edu.itba.paw.webapp.forms.ProfessionalForm;
+import com.sun.xml.internal.bind.v2.TODO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -32,6 +33,7 @@ public class RegistrationController {
 
         if(errors.hasErrors() || !personalForm.matchingPasswords(personalForm.getPassword(), personalForm.getPasswordConfirmation())){
             if(!personalForm.matchingPasswords(personalForm.getPassword(), personalForm.getPasswordConfirmation())){
+                /*TODO: this doesn't show the error message*/
                 showDoctorRegistration(personalForm).addObject("noMatchingPassword", true);
             }
             return showDoctorRegistration(personalForm);
@@ -41,6 +43,9 @@ public class RegistrationController {
             mav.addObject("professional", new ProfessionalForm());
             mav.addObject("insuranceList", searchService.listInsurances().get());
             mav.addObject("insurancePlan", searchService.listInsurancePlan().get());
+
+            /*TODO: habria que agregarle un campo a este create doctor que se settee en profileNotCompleted*/
+            /*TODO: pasar el id de doctor por parametro URL asi despues en profile agarro el doctor de ese ID y hago sets de toda la info del profile*/
 
             doctorService.createDoctor(personalForm.getFirstName(), personalForm.getLastName(), personalForm.getPhoneNumber(),
                      personalForm.getSex(), personalForm.getLala(), "null", personalForm.getAddress());
@@ -72,6 +77,12 @@ public class RegistrationController {
         if(errors.hasErrors()){
             return showDoctorProfile(professionalForm);
         }
+        System.out.println(professionalForm.getLanguages());
+
+        /*TODO: aca tenemos que llamar a una funcion de professionalForm que lo que haga es pasar ese String de Languages a una lista, easy peasy, para despues poder pasarselo bien al doctor.*/
+        /*TODO: recibir el id del doctor creado recientemente, o que este loggeado para poder hacer los sets de toda la info nueva*/
+        /*TODO: poner el valor de profileCompleted en true, asi ya se puede mostrar en la pantalla*/
+
         final ModelAndView mav = new ModelAndView("finalStep");
         return mav;
     }
