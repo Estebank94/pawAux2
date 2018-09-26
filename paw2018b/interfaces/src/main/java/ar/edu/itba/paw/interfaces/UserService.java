477519a7a16909968e58e5663ff880689b50c13e
Oliver <oliverbalfour@gmail.com>
2018-09-26 15:52:13 -0300

    new services for user and patient

--- paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserService.java
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserService.java
@@ -5,4 +5,6 @@ import ar.edu.itba.paw.models.User;
 public interface UserService {
 
 	public User findById(Integer id);
+
+	public User createUser(Integer id, String password, String email, String role);
 }
