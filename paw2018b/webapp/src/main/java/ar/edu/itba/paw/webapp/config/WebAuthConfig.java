9fd215fd29e1b73faae79d5b76b6e46ee5a3272a
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-26 21:49:36 -0300

    filters

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -31,7 +31,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+//      auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
 
         //add our users for in memory authentication
         auth.inMemoryAuthentication().withUser("martina").password("martina").roles("DOCTOR");
