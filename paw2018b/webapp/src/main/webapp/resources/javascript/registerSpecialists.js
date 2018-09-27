e5de2216d45293779fe579c321bf314a8ce81d8f
Esteban <Estebankramer@hotmail.com>
2018-09-27 15:21:33 -0300

    Functions for registerSpecialist

---
+++ paw2018b/webapp/src/main/webapp/resources/javascript/registerSpecialists.js
@@ -0,0 +1,79 @@
+/**
+ * Created by estebankramer on 27/09/2018.
+ */
+
+function classConcatenator(val){
+    var index = val.indexOf(" ");
+    var val2 = "";
+    if(index > 0){
+        var aux;
+        aux = val.slice(0,index);
+        val2 = "."+val.slice(index+1, val.length);
+        val = aux;
+        val=val+val2;
+    }
+    return val;
+}
+
+function removeSpaces(val){
+    return val.split(' ').join('');
+}
+
+function myFunc(val) {
+    var container = classConcatenator(val);
+    $("#insuranceContainer").children().hide();
+    $("."+container).show();
+}
+
+
+function addInput(val, container, name){
+    var id = removeSpaces(val);
+    if(val!== "no" &&  $("#" + id).length === 0){
+        $('#profile').append('<input type="hidden" name="'+name+'" value="'+val+'" class="'+id+'" id="'+name+'"/>');
+        $('#'+ container).append('<button type="button" class="btn btn-primary"  id="'+id+'" style="margin-right: 8px; margin-bottom: 8px">'+
+            val + '<span style="margin-right: 4px; margin-left: 8px"><i class="fas fa-times-circle">'+'</i></span></button>');
+
+    }
+}
+
+function addInputSelect(){
+    var insurance = $("#insurance").val();
+    var id = removeSpaces(insurance);
+    insurance = classConcatenator(insurance);
+
+    var selected = [];
+    $('.'+insurance+' input:checked').each(function() {
+        selected.push($(this).attr('value'));
+    });
+
+    if(insurance!== "no" && $("#" + id).length === 0 && selected.length > 0 ){
+        $('#profile').append('<input type="hidden" name="insurancePlan"  class="'+id+'" value="' + selected + '" id="insurancePlan"/>');
+        $('#profile').append('<input type="hidden" name="insurance" value="'+insurance+'" class="'+id+'" id="insurance"/>');
+        $('#addedInsurances').append('<button type="button" class="btn btn-primary" id="'+id+'" style="margin-right: 8px; margin-bottom: 8px;">'+
+            $("#insurance").val() + '<span class="badge badge-light" style="margin-left: 8px; margin-right: 4px;">'+ selected.length +'</span><span style="margin-right: 4px; margin-left: 8px"><i class="fas fa-times-circle">'+'</i></span></button>');
+    }
+}
+
+$("#addedInsurances").on("click", ".btn", function(button){
+    var id = button.target.id;
+    $('#'+id).remove();
+    $('#profile').children('.'+id).remove();
+});
+
+$("#insuranceContainer").on("click", ".btn", function(button){
+    var id = button.target.id;
+    $('#'+id).remove();
+    $('#profile').children('.'+id).remove();
+});
+
+$("#languageContainer").on("click", ".btn", function(button){
+    var id = button.target.id;
+    $('#'+id).remove();
+    $('#profile').children('.'+id).remove();
+});
+
+$("#addedSpecialties").on("click", ".btn", function(button){
+    var id = button.target.id;
+    $('#'+id).remove();
+    $('#profile').children('.'+id).remove();
+});
