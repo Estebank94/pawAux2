5824ef947c06f6c1ebf7166c2909b1d1f15654cb
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-29 15:26:00 -0300

    doctor panel controller

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/DoctorPanelController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/DoctorPanelController.java
@@ -1,21 +1,57 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.App;
+import ar.edu.itba.paw.interfaces.DoctorService;
+import ar.edu.itba.paw.models.Appointment;
+import ar.edu.itba.paw.models.Doctor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 
 @RequestMapping("/doctorPanel")
 @Controller
 public class DoctorPanelController {
+
+    @Autowired
+    private DoctorService doctorService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DoctorPanelController.class);
 
     @RequestMapping("/")
     public ModelAndView doctorPanel(){
         ModelAndView mav = new ModelAndView("doctorPanel");
+
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+////        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+////            String currentUserName = authentication.getName();
+////            Doctor doctor = (Doctor) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+////            Map<LocalDate, List<Appointment>> appointments = doctor.getAvailableAppointments();
+////        }
+
+        Doctor doctor = doctorService.findDoctorById(32).get();
+
+        Map<LocalDate, List<LocalTime>> appointments = doctor.appointmentsToMap();
+
+        mav.addObject("appointments", appointments);
+        mav.addObject("doctor", doctor);
+
         return mav;
+
     }
 
 
