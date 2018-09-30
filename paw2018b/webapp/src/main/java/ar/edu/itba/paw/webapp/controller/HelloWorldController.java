004cb4da481307a5571a12b1b81dea694fb53371
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-09-30 17:25:47 -0300

    deleted inputs

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -9,21 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import ar.edu.itba.paw.interfaces.UserService;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-import javax.validation.Valid;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.*;
 
 @Controller
