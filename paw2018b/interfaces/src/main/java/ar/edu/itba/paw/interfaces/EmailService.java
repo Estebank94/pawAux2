cf55f7abd06d0b678147de4c69a094a53219792e
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-10-02 18:30:34 -0300

    no message

---
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/EmailService.java
@@ -0,0 +1,6 @@
+package ar.edu.itba.paw.interfaces;
+
+public interface EmailService {
+
+   void sendEmail (String to, String subject, String text);
+}
---
+++ paw2018b/services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -0,0 +1,52 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.interfaces.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    public JavaMailSender jms;
+
+    @Async
+    @Override
+    public void sendEmail(String recipient, String body, String subject) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(recipient);
+        mail.setSubject(subject);
+        mail.setText(body);
+        jms.send(mail);
+    }
+
+//    @Override
+//    public void sendReservationEmail(Transaction transaction, User user, TransactionRequest tr) {
+//        sendEmail(user.getEmail(),buildEmailString(transaction, user, tr),"Tu compra de entradas");
+//    }
+//
+//    private String buildEmailString(Transaction transaction, User user, TransactionRequest tr){
+//        StringBuilder sb = new StringBuilder();
+//
+//        sb.append("¡Hola ");
+//        sb.append(user.getName());
+//        sb.append("! Aquí están los detalles de tu compra: \n Asientos ");
+//        for(String s : tr.getSeatNames()){
+//            sb.append(s);
+//        }
+//
+//        sb.append("\n");
+//        for(String s : tr.getFoodDetails()){
+//            sb.append(s);
+//            sb.append("\n");
+//        }
+//
+//        sb.append("Total: ");
+//        sb.append(transaction.getPrice());
+//
+//        return sb.toString();
+//    }
+}
