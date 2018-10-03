f8883d15abe263b793d4be67eeae02ac3d07fc94
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-10-03 13:02:52 -0300

    email

--- paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/EmailService.java
+++ paw2018b/interfaces/src/main/java/ar/edu/itba/paw/interfaces/EmailService.java
@@ -2,5 +2,9 @@ package ar.edu.itba.paw.interfaces;
 
 public interface EmailService {
 
-   void sendEmail (String to, String subject, String text);
+   void sendEmail(String to, String subject, String text);
+
+   public String prepareConfirmationMessage(String name);
+
+   void sendMessageWithAttachment(String name, String to, String subject);
 }
