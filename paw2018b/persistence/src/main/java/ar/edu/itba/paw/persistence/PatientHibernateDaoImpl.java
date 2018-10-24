86413fa8edaa5f83044bdb0dbe35687f76f2c79c
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-10-24 20:35:18 -0300

    findby has to be with query

--- paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/PatientHibernateDaoImpl.java
+++ paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/PatientHibernateDaoImpl.java
@@ -6,17 +6,17 @@ import ar.edu.itba.paw.models.Patient;
 import ar.edu.itba.paw.models.exceptions.NotCreatePatientException;
 import ar.edu.itba.paw.models.exceptions.NotFoundDoctorException;
 import ar.edu.itba.paw.models.exceptions.RepeatedEmailException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
-
-/**
- * Created by estebankramer & Palito on 10/10/2018.
- */
 
 @Repository
 public class PatientHibernateDaoImpl implements PatientDao {
@@ -25,9 +25,14 @@ public class PatientHibernateDaoImpl implements PatientDao {
     @PersistenceContext
     private EntityManager em;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
+//    @Transactional
     public Patient createPatient(String firstName, String lastName, String phoneNumber, String email, String password) throws RepeatedEmailException {
-        final Patient patient = new Patient(firstName,lastName,phoneNumber, email, password);
+        String finalpassword = passwordEncoder.encode(password);
+        final Patient patient = new Patient(firstName,lastName,phoneNumber, email, finalpassword);
         em.persist(patient);
         return patient;
     }
@@ -43,8 +48,11 @@ public class PatientHibernateDaoImpl implements PatientDao {
     }
 
     @Override
-    public Optional<Patient> findPatientByEmail(String email) {
-        return Optional.ofNullable(em.find(Patient.class, email));
+    public Patient findPatientByEmail(String email) {
+        final TypedQuery<Patient> query = em.createQuery("FROM Patient as p where p.email = :email", Patient.class);
+        query.setParameter("email", email);
+        final List<Patient> list = query.getResultList();
+        return list.isEmpty() ? null : list.get(0);
     }
 
     private List<Appointment> findPacientAppointmentsById(Integer id){
