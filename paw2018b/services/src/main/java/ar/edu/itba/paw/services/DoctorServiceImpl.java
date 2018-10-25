244ba1e9b73dd5369303f9a18c160f0c5ef4c329
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-10-25 09:05:51 -0300

    @transactional

--- paw2018b/services/src/main/java/ar/edu/itba/paw/services/DoctorServiceImpl.java
+++ paw2018b/services/src/main/java/ar/edu/itba/paw/services/DoctorServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sun.rmi.runtime.Log;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.security.acl.LastOwnerException;
 import java.sql.SQLException;
 import java.util.List;
@@ -23,12 +25,14 @@ import java.util.Set;
 @Service("DoctorServiceImpl")
 public class DoctorServiceImpl implements DoctorService {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Autowired
     private DoctorDao doctorDao;
     
     @Autowired
     private SpecialtyDao specialtyDao;
-
 
     @Autowired
     private InsurancePlanDao insurancePlanDao;
@@ -85,7 +89,8 @@ public class DoctorServiceImpl implements DoctorService {
 
 
     @Override
-    public Doctor createDoctor(String firstName, String lastName, String phoneNumber, String sex, String licence,
+    @Transactional
+    public Doctor createDoctor(String firstName, String lastName, String phoneNumber, String sex, Integer licence,
                                String avatar, String address) throws NotValidFirstNameException, NotValidLastNameException, NotValidPhoneNumberException, NotCreateDoctorException, RepeatedLicenceException, NotValidSexException, NotValidLicenceException, NotValidAddressException {
         LOGGER.debug("DoctorServiceImpl: createDoctor");
         if (firstName == null){
@@ -162,19 +167,19 @@ public class DoctorServiceImpl implements DoctorService {
             LOGGER.debug("The Licence of a Doctor can't be null");
             throw new NotValidLicenceException("licence can't be null");
         }
-        if (licence.length() == 0){
+        if (licence <= 0){
             LOGGER.debug("The Licence of a Doctor can't have 0 characters");
             throw new NotValidLicenceException("licence can't be empty");
         }
 
-        if (licence.length() >= 10){
+        if (licence >= Integer.MAX_VALUE){
             LOGGER.debug("The Licence of a Doctor can't have more than 9 characters. The Licence given is: {}", licence);
             throw new NotValidLicenceException("licence can't have more than 10 characters");
         }
-        if (Integer.valueOf(licence) > Integer.MAX_VALUE){
-            LOGGER.debug("The Licence of a Doctor can't have more than 9 characters. The Licence given is: {}", licence);
-            throw new NotValidLicenceException("licence can't have more than 10 characters");
-        }
+//        if (Integer.valueOf(licence) > Integer.MAX_VALUE){
+//            LOGGER.debug("The Licence of a Doctor can't have more than 9 characters. The Licence given is: {}", licence);
+//            throw new NotValidLicenceException("licence can't have more than 10 characters");
+//        }
 
         if (address == null) {
             LOGGER.debug("The address of a Doctor can't be null");
