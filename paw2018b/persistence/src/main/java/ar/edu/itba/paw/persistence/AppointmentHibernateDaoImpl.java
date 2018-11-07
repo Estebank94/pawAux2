befe90db799e7d4c14d19fc0076484b77c9cc8a6
Juan Pablo Lo Coco <jlococo@itba.edu.ar>
2018-11-07 17:36:29 -0300

    cancelappointment is done

--- paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/AppointmentHibernateDaoImpl.java
+++ paw2018b/persistence/src/main/java/ar/edu/itba/paw/persistence/AppointmentHibernateDaoImpl.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Optional;
 
 /**
  * Created by estebankramer on 13/10/2018.
@@ -26,5 +28,27 @@ public class AppointmentHibernateDaoImpl implements AppointmentDao {
         final Appointment appointment = new Appointment(appointmentDay, appointmentTime, patient, doctor);
         em.persist(appointment);
         return appointment;
+    }
+
+    @Override
+    public void cancelAppointment(Appointment appointment){
+        appointment.setAppointmentCancelled(true);
+        em.persist(appointment);
+    }
+
+    @Override
+    public Optional<Appointment> findAppointment (String appointmentDay, String appointmentTime, Patient patient, Doctor doctor) throws Exception{
+        final TypedQuery<Appointment> query = em.createQuery("from Appointment as a WHERE a.appointmentDay = :appointmentDay AND a.appointmentTime = :appointmentTime AND a.patient = :patient AND a.doctor = :doctor",Appointment.class);
+        query.setParameter("appointmentDay", appointmentDay);
+        query.setParameter("appointmentTime", appointmentTime);
+        query.setParameter("patient", patient);
+        query.setParameter("doctor", doctor);
+        return Optional.ofNullable(query.getSingleResult());
+    }
+
+    @Override
+    public void undoCancelAppointment(Appointment appointment) {
+        appointment.setAppointmentCancelled(false);
+        em.persist(appointment);
     }
 }
