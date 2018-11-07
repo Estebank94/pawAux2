00f6a78764ff40d0dcc501ad26518fec289850fe
Oliver <oliverbalfour@gmail.com>
2018-11-07 14:33:31 -0300

    appointment service test fixed, compare with mock daos instead of real ones

--- paw2018b/services/src/test/java/ar/edu/itba/paw/services/AppointmentServiceImplTest.java
+++ paw2018b/services/src/test/java/ar/edu/itba/paw/services/AppointmentServiceImplTest.java
@@ -1,72 +1,119 @@
-//package ar.edu.itba.paw.services;
+
+
+package ar.edu.itba.paw.services;
+
+
+import ar.edu.itba.paw.interfaces.AppointmentDao;
+import ar.edu.itba.paw.interfaces.DoctorDao;
+import ar.edu.itba.paw.models.Appointment;
+import ar.edu.itba.paw.models.Doctor;
+import ar.edu.itba.paw.models.Patient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@Sql("classpath:ServiceTest.sql")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+public class AppointmentServiceImplTest {
+
+    private static final LocalTime TIME = LocalTime.of(9, 10, 50);
+    private static final LocalDate DATE = LocalDate.of(2017, Month.JANUARY, 1);
+    private static final Integer DOC_ID = 1;
+    private static final Integer PATIENT_ID = 5;
+
+//    @Rule
+//    public MockitoRule rule = MockitoJUnit.rule();
 //
+//    @InjectMocks
+//    private AppointmentDao appointmentDao;
 //
-//import ar.edu.itba.paw.models.Appointment;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.jdbc.Sql;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.jdbc.JdbcTestUtils;
-//
-//import javax.sql.DataSource;
-//import java.time.DayOfWeek;
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.time.Month;
-//import java.util.Optional;
-//
-//import static org.junit.Assert.*;
-//
-//@Sql("classpath:ServiceTest.sql")
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = TestConfig.class)
-//public class AppointmentServiceImplTest {
-//
-//    private static final LocalTime TIME = LocalTime.of(9, 10, 50);
-//    private static final LocalDate DATE = LocalDate.of(2017, Month.JANUARY, 1);
-//    private static final Integer DOC_ID = 1;
-//    private static final Integer PATIENT_ID = 5;
-//
-//    @Autowired
-//    private AppointmentServiceImpl appointmentServiceImpl;
-//
-//    @Autowired
-//    private DataSource ds;
-//
-//    private JdbcTemplate jdbcTemplate;
-//
-//    @Before
-//    public void setUp() {
-//
-//        jdbcTemplate = new JdbcTemplate(ds);
-//
-//    }
-//
-//    @After
-//    public void tearDown(){
-//        JdbcTestUtils.deleteFromTables(jdbcTemplate, "appointment", "workingHour", "medicalCare", "doctorSpecialty", "doctor", "insurancePlan", "insurance",
-//                "Specialty", "review", "information", "patient");
-//    }
-//
-//    @Test
-//    public void testCreateAppointment() throws Exception {
-//
-//        System.out.println(DATE);
-//        System.out.println(TIME.toString());
-//
-//        final Optional<Appointment> appointment = appointmentServiceImpl.createAppointment(DOC_ID, PATIENT_ID, DATE, TIME);
-//
-//        assertTrue(appointment.isPresent());
-//        assertEquals(DATE, appointment.get().getAppointmentDay());
-//        assertEquals(TIME, appointment.get().getAppointmentTime());
-//        assertEquals(DOC_ID, appointment.get().getDoctorId());
-//        assertEquals(PATIENT_ID, appointment.get().getClientId());
-//
-//    }
-//
-//}
+//    @InjectMocks
+//    private DoctorDao doctorDao;
+
+    @Autowired
+    private AppointmentServiceImpl appointmentServiceImpl;
+
+    @Autowired
+    private Doctor doctor;
+
+    @Autowired
+    private Patient patient;
+
+    @Autowired
+    private Appointment appointment;
+
+    @Autowired
+    private AppointmentDao appointmentDao;
+
+    @Autowired
+    private DataSource ds;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp() {
+
+        jdbcTemplate = new JdbcTemplate(ds);
+//        MockitoAnnotations.initMocks(this);
+        doctor = Mockito.mock(Doctor.class);
+        patient = Mockito.mock(Patient.class);
+        when(appointment.getAppointmentDay()).thenReturn(DATE.toString());
+        when(appointment.getAppointmentTime()).thenReturn(TIME.toString());
+        when(appointment.getPatient()).thenReturn(patient);
+        when(appointment.getDoctor()).thenReturn(doctor);
+        when(patient.getId()).thenReturn(PATIENT_ID);
+        when(doctor.getId()).thenReturn(DOC_ID);
+
+        try {
+            when(appointmentDao.createAppointment(DATE.toString(), TIME.toString(), patient, doctor)).thenReturn(appointment);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @After
+    public void tearDown(){
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "appointment", "workingHour", "medicalCare", "doctorSpecialty", "doctor", "insurancePlan", "insurance",
+                "Specialty", "review", "information", "patient");
+    }
+
+    @Test
+    public void testCreateAppointment() throws Exception {
+
+        final Appointment appointment = appointmentServiceImpl.createAppointment(DATE.toString(), TIME.toString(), patient, doctor);
+
+        assertEquals(DATE.toString(), appointment.getAppointmentDay());
+        assertEquals(TIME.toString(), appointment.getAppointmentTime());
+        assertEquals(DOC_ID, appointment.getDoctor().getId());
+        assertEquals(PATIENT_ID, appointment.getPatient().getId());
+
+    }
+
+}
--- paw2018b/services/src/test/java/ar/edu/itba/paw/services/TestConfig.java
+++ paw2018b/services/src/test/java/ar/edu/itba/paw/services/TestConfig.java
@@ -1,7 +1,14 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.*;
+import ar.edu.itba.paw.models.Appointment;
+import ar.edu.itba.paw.models.Doctor;
+import ar.edu.itba.paw.models.Patient;
 import org.hsqldb.jdbc.JDBCDriver;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +18,136 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.Properties;
 
 import static org.mockito.Mockito.when;
 
-@ComponentScan({ "ar.edu.itba.paw.services", "ar.edu.itba.paw.persistence" })
+@ComponentScan({ "ar.edu.itba.paw.services", })
 @Configuration
 public class TestConfig {
+
+    public TestConfig() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Mock
+    private Doctor doctor;
+
+    @Mock
+    private Patient patient;
+
+    @Mock
+    private Appointment appointment;
+
+    @Mock
+    private AppointmentDao appointmentDao;
+
+    @Mock
+    private DoctorDao doctorDao;
+
+    @Mock
+    private SpecialtyDao specialtyDao;
+
+    @Mock
+    private InsurancePlanDao insurancePlanDao;
+
+    @Mock
+    private DescriptionDao descriptionDao;
+
+    @Mock
+    private WorkingHoursDao workingHoursDao;
+
+    @Mock
+    private FavoriteDao favoriteDao;
+
+    @Mock
+    private PatientDao patientDao;
+
+    @Mock
+    private ReviewDao reviewDao;
+
+    @Mock
+    private SearchDao searchDao;
+
+    @Mock
+    private EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    @Bean
+    public Patient getPatient() {
+        return patient;
+    }
+
+    @Bean
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    @Bean
+    public DoctorDao getDoctorDao() {
+        return doctorDao;
+    }
+
+    @Bean
+    public AppointmentDao getAppointmentDao() throws Exception{
+        return appointmentDao;
+    }
+
+    @Bean
+    public SpecialtyDao getSpecialtyDao() {
+        return specialtyDao;
+    }
+
+    @Bean
+    public InsurancePlanDao getInsurancePlanDao() {
+        return insurancePlanDao;
+    }
+
+    @Bean
+    public DescriptionDao getDescriptionDao() {
+        return descriptionDao;
+    }
+
+    @Bean
+    public WorkingHoursDao getWorkingHoursDao() {
+        return workingHoursDao;
+    }
+
+    @Bean
+    public FavoriteDao getFavoriteDao() {
+        return favoriteDao;
+    }
+
+    @Bean
+    public PatientDao getPatientDao() {
+        return patientDao;
+    }
+
+    @Bean
+    public ReviewDao getReviewDao() {
+        return reviewDao;
+    }
+
+    @Bean
+    public SearchDao getSearchDao() {
+        return searchDao;
+    }
+
+    @Bean
+    public EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
     @Bean
     public DataSource dataSource() {
         final SimpleDriverDataSource ds = new SimpleDriverDataSource();
