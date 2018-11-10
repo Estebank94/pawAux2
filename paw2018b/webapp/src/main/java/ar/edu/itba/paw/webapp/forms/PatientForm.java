47b2440bc65b9699ad659ab6e4b0c01109f59585
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-11-10 15:01:15 -0300

    mssg

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/forms/PatientForm.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/forms/PatientForm.java
@@ -11,19 +11,19 @@ public class PatientForm {
 
     @Length(min=3, max=45, message = "Cantidad de caracteres incorrecto. Su nombre debe contener entre 3 y 45 caracteres")
     @NotEmpty(message = "Este campo es obligatorio. Por favor, ingrese su nombre")
-    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Su nombre no puede contener simbolos que no sean letras y especios")
+    @Pattern(regexp = "^[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]*$", message = "Su nombre no puede contener simbolos que no sean letras y espacios")
     private String firstName;
 
     @Length(min=2, max=45, message = "Cantidad de caracteres incorrecto. Su apellido debe contener entre 2 y 45 caracteres")
     @NotEmpty(message = "Este campo es obligatorio. Por favor, ingrese su apellido")
-    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Su apellido no puede contener simbolos que no sean letras y especios")
+    @Pattern(regexp = "^[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]*$", message = "Su nombre no puede contener simbolos que no sean letras y espacios")
     private String lastName;
 
     @Email(message = "El email ingresado es invalido. Por favor, ingrese un email valido")
     @NotEmpty(message = "Este campo es obligatorio. Por favor, ingrese su email")
     private String email;
 
-    @Length(min=6, max=55, message = "Cantidad de caracteres incorrecto. Su contraseña debe contener entre 6 y 55 caracteres")
+    @Length(min=6, max=55, message = "Cantidad de caracteres incorrecto. Su contraseña debe contener entre 6 y 55 caracteres y un número")
     @NotEmpty(message = "Este campo es obligatorio.Por favor, ingrese una contraseña")
     @Pattern(regexp = "[a-zA-Z0-9]+", message = "Su contraseña debe contener al menos un número. Por favor, reingrese una contraseña valida")
     private String password;
@@ -35,7 +35,6 @@ public class PatientForm {
 
 
     @Pattern(regexp = "^((\\(|)(011)(\\)|)(|\\s)(\\d{8}|\\d{4}\\s\\d{4}))$", message = "Su numero de telefono es incorrecto. Recuerde que el formato es : (Codigo de Area) Numero de Telefono ")
-    @NotNull(message = "Este campo es obligatorio. Por favor, ingrese su número de teléfono")
     @NotEmpty(message = "Este campo es obligatorio. Por favor, ingrese su número de teléfono")
     private String phoneNumber;
 
