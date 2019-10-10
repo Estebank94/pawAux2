ee8679fb344f99b2c8a4880a2985afdf9fe39ce5
Esteban Kramer <estebankramer@gmail.com>
2019-10-10 17:26:32 -0300

    Fade

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import { render, } from 'react-dom'
+import { render } from 'react-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const target = document.querySelector('#root')
-
 render(
   <BrowserRouter>
     <App />
