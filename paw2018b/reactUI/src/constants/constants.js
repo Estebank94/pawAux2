d279ab8b503ee378a64a91b3e2457e2f528bdf3e
Esteban Kramer <estebankramer@gmail.com>
2019-10-14 18:53:01 -0300

    Fetch api function

---
+++ paw2018b/reactUI/src/constants/constants.js
@@ -0,0 +1,6 @@
+/**
+ * Created by estebankramer on 14/10/2019.
+ */
+//BACKEND URL
+
+export const API_URL = 'http://localhost:8080/api';
---
+++ paw2018b/reactUI/src/utils/api.js
@@ -0,0 +1,33 @@
+/**
+ * Created by estebankramer on 14/10/2019.
+ */
+
+import axios from "axios";
+import { API_URL } from "../constants/constants";
+
+//TODO Remove cross origin headers
+export default (endpoint, method, body = {}) => {
+    switch (endpoint) {
+        default:
+            if (method === 'GET') {
+                return axios({
+                    url: API_URL + endpoint,
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                    }
+                }).then(response => response.data)
+            } else {
+                return axios({
+                    url: API_URL + endpoint,
+                    method: method,
+                    data: body,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                    }
+                }).then(response => response.data)
+            }
+    }
+}
