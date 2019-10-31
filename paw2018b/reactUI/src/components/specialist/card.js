03a2af3e0a666d0e8c074d6790a7f4d1cbbad1cb
Esteban Kramer <estebankramer@gmail.com>
2019-10-31 17:29:47 -0300

    [SPECIALISTS] Get specialists from DB and display them

--- paw2018b/reactUI/src/components/specialist/card.js
+++ paw2018b/reactUI/src/components/specialist/card.js
@@ -2,24 +2,38 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 class SpecialistCard extends React.Component {
-  render() {
+
+    renderRating = () => {
+        const { averageRating } = this.props.data;
+        let stars = [];
+
+        // Outer loop to create parent
+        for (let i = 0; i < averageRating; i++) {
+            stars.push(<i className="fas fa-star star-yellow"></i>);
+        }
+        for(let j = averageRating + 1; j < 5; j++){
+            stars.push(<i className="fas fa-star star-grey"></i>)
+        }
+        return stars;
+    }
+
+    render() {
+      const { profilePicture, firstName, lastName, phoneNumber, specialties, averageRating } = this.props.data;
     return(
       <div className="card card-doctor d-flex flex-row box">
-          <img src="https://d1k13df5m14swc.cloudfront.net/photos/Dr-Steven-Radowitz-MD-413-circle_medium__v1__.png" className="avatar" />
+          <img class="avatar big" src={`data:image/jpeg;base64,${profilePicture}`} className="avatar" />
           <div className="card-body">
               <div className="card-text">
-                  <h3 className="doctor-name">Nombre Apellido</h3>
+                  <h3 className="doctor-name">{firstName} {lastName}</h3>
                   <div className="row container">
-                      <p className="doctor-specialty" style={{ paddingRight: 20 }}>Especialidades</p>
+                      <p className="doctor-specialty" style={{ paddingRight: 20 }}>{specialties.specialties.map(s => s.speciality + ' ')}</p>
                   </div>
                   <p className="doctor-text">Certificado</p>
-                  <div className="row container">
-                      <i className="fas fa-star star-yellow"></i>
-                      <i className="fas fa-star star-yellow"></i>
-                      <i className="fas fa-star star-yellow"></i>
-                      <i className="fas fa-star star-yellow"></i>
-                      <i className="fas fa-star star-grey"></i>
-                  </div>
+                  { averageRating !== 0 &&
+                      <div className="row container">
+                          {this.renderRating()}
+                      </div>
+                  }
                   <p className="doctor-text">"Muy buena atención, muy puntual"</p>
                   <p className="doctor-text"><i className="far fa-clock"></i>8 - 20pm</p>
                   <p className="doctor-text"><i className="fas fa-map-marker-alt"></i> Libertador 1000, CABA</p>
--- paw2018b/reactUI/src/pages/specialists/index.js
+++ paw2018b/reactUI/src/pages/specialists/index.js
@@ -1,15 +1,65 @@
-import React from 'react'
+import React from 'react';
+import i18n from "../../i18n";
+import BounceLoader from 'react-spinners/BounceLoader';
+
+import fetchApi from '../../utils/api'
 
 import SpecialistCard from '../../components/specialist/card';
 
 class Specialists extends React.Component {
+    state = {
+        loading: true,
+        error: false,
+        specialists: null,
+    };
+
+    componentDidMount() {
+        // const { id } = this.props.match.params;
+        fetchApi('/doctor/list?page=' + 1, 'GET') //TODO poner bien el page del query param
+    .then(specialists => {
+            console.log(specialists);
+            this.setState({loading: false, specialists });
+        })
+            .catch(() => this.setState({ loading: false, error: true }));
+    }
+
   render() {
+      const { error, loading, specialists } = this.state;
+
+      if(loading) {
+          return (
+              <div className="align-items-center justify-content-center">
+                  <BounceLoader
+                      sizeUnit={"px"}
+                      size={75}
+                      color={'rgb(37, 124, 191)'}
+                      loading={true}
+                  />
+              </div>
+          )
+      }
+
+      if(error) {
+          return (
+              <p>Hubo un error!</p>
+          )
+      }
+
     return (
       <div className="body-background">
-        <div className="main-container">
-          <div className="col-md-9">
-            <SpecialistCard />
-          </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-9">
+                    {
+                        specialists.doctors.map(specialist => <SpecialistCard data={specialist} />)
+                    }
+                </div>
+                <div className="col-md-3">
+                    <div class="sidebar-nav-fixed pull-right affix">
+                        <h3 class="sidebar-title">{i18n.t('prueba')}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     )
