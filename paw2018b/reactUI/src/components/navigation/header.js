0c8de5bb99ff2c4fcef131a605cfde95360f60c7
Esteban Kramer <estebankramer@gmail.com>
2019-11-30 16:43:01 -0300

    [Header] Style changes

--- paw2018b/reactUI/src/components/navigation/header.js
+++ paw2018b/reactUI/src/components/navigation/header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 class Navigation extends React.Component {
   handleClick() {
-    const { id } = this.props.data;
     this.props.history.push(`/login`);
   }
 
@@ -15,19 +15,21 @@ class Navigation extends React.Component {
             <a className="navbar-brand" href="/">
                 <h1 className={pathname === '/' ? 'navbar-brand-home' : ''}><strong>Waldoc</strong></h1>
             </a>
-            <a href="/login">
-                <div className="row">
-                    <div className="center-vertical">
-                      <button
-                        onClick={() => this.handleClick()}
-                        className="btn btn-light" style={{ backgroundColor: 'transparent', borderColor: 'transparent', color: '#257CBF' }}
-                        type="button"
-                      >
-                        Iniciar Sesion
-                      </button>
-                    </div>
-                </div>
-            </a>
+          <div className="row">
+            <DropdownButton id="dropdown-basic-button btn-register" variant="light" title="Registrarse">
+              <Dropdown.Item href="/register">Registrarse como paciente</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Registrarse como especialista</Dropdown.Item>
+            </DropdownButton>
+            <div className="center-vertical">
+              <button
+                onClick={() => this.handleClick()}
+                className="btn btn-light" style={{ backgroundColor: 'transparent', borderColor: 'transparent', color: pathname === '/' ? '#257CBF' : '#FFF' }}
+                type="button"
+              >
+                Iniciar Sesion
+              </button>
+            </div>
+          </div>
         </div>
       </nav>
     );
