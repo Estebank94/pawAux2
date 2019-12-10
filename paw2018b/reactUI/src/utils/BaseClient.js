676dd56aff5dec0cddf474ccf76d2b143d5b286f
Esteban Kramer <estebankramer@gmail.com>
2019-12-10 14:24:39 -0300

    [API] Improved api client

---
+++ paw2018b/reactUI/src/utils/BaseClient.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+class BaseClient {
+  constructor(props) {
+    console.log('PROPS', props);
+
+    let token;
+    let dispatch;
+    if (props && props.user) {
+      token = props.user.auth;
+      // dispatch = props.dispatch;
+    }
+
+    const headers = {
+      'X-AUTH-TOKEN': token
+    };
+
+    // Create axios instance
+    this.instance = axios.create({
+      baseURL: 'http://localhost:8080/api/v1',
+      headers,
+    });
+
+    // Add a response interceptor for 401
+    this.instance.interceptors.response.use(
+      (response) => {
+        return response;
+      }, (error) => {
+        let errorResponse = error.response;
+        if (errorResponse.status === 401) {
+          // dispatch({type: LOGOUT});
+          props.history.push('login');
+        }
+        if (errorResponse.status === 403) {
+          // dispatch({type: LOGOUT});
+          props.history.push('login');
+        }
+        if (errorResponse.status === 500) {
+          // dispatch({type: LOGOUT});
+          props.history.push('500');
+        }
+        return Promise.reject(error);
+      });
+  }
+}
+
+export default BaseClient;
