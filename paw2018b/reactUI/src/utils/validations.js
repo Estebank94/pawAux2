af36ebbfcb2aae91169b87f33b1b8adc5180fa8c
Esteban Kramer <estebankramer@gmail.com>
2019-12-14 19:04:04 -0300

    [CompleteProfile] Fix call of incorrect function

--- paw2018b/reactUI/src/utils/validations.js
+++ paw2018b/reactUI/src/utils/validations.js
@@ -1,6 +1,5 @@
 export const isValidEmail = (email) => {
   const validEmailRegex =
     RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
-  console.log(validEmailRegex.test(email));
   return validEmailRegex.test(email)
 }
