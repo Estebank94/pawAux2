e2bbf893f39894781cb186ea5f1956788ec3f565
Esteban Kramer <estebankramer@gmail.com>
2019-12-14 16:28:15 -0300

    [BaseClient] Handle error improvements

--- paw2018b/reactUI/src/utils/BaseClient.js
+++ paw2018b/reactUI/src/utils/BaseClient.js
@@ -2,13 +2,11 @@ import axios from 'axios';
 
 class BaseClient {
   constructor(props) {
-    console.log('PROPS', props);
 
     let token;
-    let dispatch;
     if (props && props.user) {
       token = props.user.auth;
-      // dispatch = props.dispatch;
+
     }
 
     const headers = {
@@ -16,29 +14,27 @@ class BaseClient {
       'Content-Type': 'application/json'
     };
 
-    // Create axios instance
     this.instance = axios.create({
       baseURL: 'http://localhost:8080/api/v1',
       timeout: 60000,
       headers,
     });
 
-    // Add a response interceptor for 401
     this.instance.interceptors.response.use(
       (response) => {
         return response;
       }, (error) => {
         let errorResponse = error.response;
+        if(!errorResponse) {
+          props.history.push('/error/Error');
+        }
         if (errorResponse.status === 401) {
-          // dispatch({type: LOGOUT});
-          props.history.push('login');
+          props.history.push('/error/401');
         }
         if (errorResponse.status === 403) {
-          // dispatch({type: LOGOUT});
-          props.history.push('login');
+          props.history.push('/error/403');
         }
         if (errorResponse.status === 500) {
-          // dispatch({type: LOGOUT});
           props.history.push('/error/500');
         }
         return Promise.reject(error);
