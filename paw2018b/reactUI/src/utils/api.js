1c3b99e3c7521f9c7461416dde481eefd43ae253
Esteban Kramer <estebankramer@gmail.com>
2019-12-14 22:03:30 -0300

    [API] Simplified code

--- paw2018b/reactUI/src/utils/api.js
+++ paw2018b/reactUI/src/utils/api.js
@@ -6,50 +6,19 @@ import axios from "axios";
 import queryString from 'query-string';
 import { API_URL } from "../constants/constants";
 
-//TODO Remove cross origin headers
 export default (endpoint, method, body = {}) => {
-    console.log('FETCHING FROM: ' + API_URL + endpoint);
-    switch (endpoint) {
-        default:
-            if (method === 'GET') {
-                return axios({
-                    url: API_URL + endpoint,
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Headers': 'Authorization',
-                        'Access-Control-Allow-Origin': '*',
-                    }
-                }).then(response => response.data)
-            } else if (method === 'XPOST') {
-              return axios({
-                url: API_URL + endpoint,
-                method: 'POST',
-                data: queryString.stringify(body),
-                headers: {
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                  'Access-Control-Allow-Headers': 'Authorization',
-                  'Access-Control-Allow-Origin': '*',
-                }
-              }).then(response => {
-                return (response.headers['x-auth-token'])
-              }).catch(error => {
-                console.log('error:', error);
-              });
-            } else {
-                return axios({
-                    url: API_URL + endpoint,
-                    method: method,
-                    data: queryString.stringify(body),
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Headers': 'x-access-token',
-                        'Access-Control-Allow-Origin': '*',
-                    }
-                }).then(response => {
-                    console.log('POST Response', response);
-                    return (response.data)
-                })
-            }
+  return axios({
+    url: API_URL + endpoint,
+    method: 'POST',
+    data: queryString.stringify(body),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Headers': 'Authorization',
+      'Access-Control-Allow-Origin': '*',
     }
+  }).then(response => {
+    return (response.headers['x-auth-token'])
+  }).catch(error => {
+    console.log('error:', error);
+  });
 }
